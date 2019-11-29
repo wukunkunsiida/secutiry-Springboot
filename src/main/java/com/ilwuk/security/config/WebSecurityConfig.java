049aaf5b9eb4210cef5b8daf1035fb5699c5b459
wukunkun <530193962@qq.com>
2019-11-29 17:56:27 +0800

    1`111

--- src/main/java/com/ilwuk/security/config/WebSecurityConfig.java
+++ src/main/java/com/ilwuk/security/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -20,33 +21,37 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    //定义用户信息服务（查询用户信息）
+//    //定义用户信息服务（查询用户信息）
+//
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(User.withUsername("zhangsan").password("123").authorities("p1").build());
+//        manager.createUser(User.withUsername("lisi").password("456").authorities("p2").build());
+//        return manager;
+//    }
 
-    @Bean
-    public UserDetailsService userDetailsService(){
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("zhangsan").password("123").authorities("p1").build());
-        manager.createUser(User.withUsername("lisi").password("456").authorities("p2").build());
-        return manager;
-    }
 
 
    // 密码编码器
+   // @Bean
+  //  public PasswordEncoder passwordEncoder(){
+      //  return NoOpPasswordEncoder.getInstance();
+   // }
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
 
     //安全拦截机制（最重要）
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http .authorizeRequests() .antMatchers("/r/**").authenticated()
                 .anyRequest().permitAll()
-                .and() .formLogin().successForwardUrl("/login-success");
+                .and() .formLogin().successForwardUrl("/login-success")
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
 
     }
 }
